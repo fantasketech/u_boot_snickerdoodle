641f14001c6fab94a93f11fd477175b67dba55fd
Russell Bush <bush@krtkl.com>
2018-03-10 12:09:08 -0800

    add snickerdoodle One support and cleanup defconfig and dts naming

---
+++ include/configs/snickerdoodle.h
@@ -0,0 +1,16 @@
+/*
+ * (c) Copyright 2017 krtkl inc.
+ *
+ * Configuration for snickerdoodle Development Platform
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_SNICKERDOODLE_H
+#define __CONFIG_SNICKERDOODLE_H
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_SNICKERDOODLE_H */
+
